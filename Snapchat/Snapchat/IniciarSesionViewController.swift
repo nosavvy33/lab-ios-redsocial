b2382e4a90ef57fc954bf7119ada53508f2d7a82
Leon <leon@MacBook-Air-de-Mac.local>
2018-10-24 17:41:44 -0500

    Hasta diseño de Snaps

---
+++ Snapchat/Snapchat/IniciarSesionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by Leon on 24/10/18.
+//  Copyright © 2018 Leon. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        /* if let user = user {
+         let uid = user.user.uid
+         print("UID:::" + uid)
+         }*/
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!){(user,error) in
+            if error != nil {
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: {(user,error) in
+                    if error != nil{
+                        print("We got an error:\(String(describing: error))")
+                    }else{
+                        self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
+                    }
+                })
+            }else{
+            self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
+        }
+    }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Snapchat/Snapchat/SnapsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SnapsViewController.swift
+//  Snapchat
+//
+//  Created by Leon on 24/10/18.
+//  Copyright © 2018 Leon. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func cerrarSesionTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
